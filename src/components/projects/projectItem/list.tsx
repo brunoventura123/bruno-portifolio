bf0f21f773a2eb84909afa07b2b16f119c0e0fd2
Bruno Leonardo Sancho Ventura <86660522+brunoventura123@users.noreply.github.com>
2022-05-09 16:11:13 -0300

    add new projectItem

--- src/components/projects/projectItem/list.tsx
+++ src/components/projects/projectItem/list.tsx
@@ -10,11 +10,13 @@ import quiz from '../../../assets/imgs/projectsImg/quiz.jpeg'
 import clock from '../../../assets/imgs/projectsImg/clock.jpeg'
 import climate from '../../../assets/imgs/projectsImg/climate.jpeg'
 import captura from '../../../assets/imgs/projectsImg/captura.jpeg'
+import multform from '../../../assets/imgs/projectsImg/multForm.jpeg'
 
 export const List = [
     { img: storeVirtual, title: 'Loja Virtual Completa', link: 'https://brunoventura123.github.io/lojavirtual/' },
     { img: jw, title: 'Site de vídeos e artigos', link: 'https://brunoventura123.github.io/jw.org-homepage/' },
     { img: captura, title: 'Página de Captura', link: 'https://brunoventura123.github.io/captura-curso/' },
+    { img: multform, title: 'Formulário Multi Etapas', link: 'https://brunoventura123.github.io/multform/' },
     { img: jewwlry, title: 'Loja virtual de Joias', link: 'https://brunoventura123.github.io/loja-joias/' },
     { img: storeLandpage, title: 'Landpage Loja Virtual', link: 'https://brunoventura123.github.io/landpage/' },
     { img: quiz, title: 'Quiz', link: 'https://brunoventura123.github.io/Quiz/' },
